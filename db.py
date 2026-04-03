24c1e070edd837e87a1282f6452e367c97a25a48
ISHANT-KB <ishantkrbag2007@gmail.com>
2026-04-03 19:06:44 +0530

    improved security

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
+import os
+from dotenv import load_dotenv
+from db import connect
 import psycopg2
 
-def connect():
-    return psycopg2.connect(
-        dbname="grocery_db",
-        user="postgres",
-        password="password",
-        host="localhost",
-        port="5432"
-    )
+load_dotenv()  # loads variables from .env
+
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
 
 if __name__=="__main__":
     try:
